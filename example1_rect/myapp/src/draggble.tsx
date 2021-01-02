01161518db5c1d3f0b1a12de5549ce29bbb7fd6f
dai-a <hxjsr990@yahoo.co.jp>
2021-01-02 16:51:39 +0900

    modify: javascript -> typescript

--- example1_rect/myapp/src/draggble.tsx
+++ example1_rect/myapp/src/draggble.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
 
-export default class Rect extends React.Component {
-  constructor() {
-    super();
-    this.state = { rect: { x: 0, y: 0 } };
+interface Props {
+  rect: { x: number; y: number };
+  dragOffset: { x: number; y: number };
+}
+
+export default class Rect extends React.Component<{}, Props> {
+  private svg: SVGSVGElement | undefined;
+  private svgRectElem: SVGRectElement | undefined;
+  constructor(props: Props) {
+    super(props);
+    this.state = { rect: { x: 0, y: 0 }, dragOffset: { x: 0, y: 0 } };
   }
 
   render() {
     return (
       <div>
-        <svg viewBox="0 0 100 100" ref={(svg) => (this.svg = svg)}>
+        <svg
+          viewBox="0 0 100 100"
+          ref={(svg: SVGSVGElement) => (this.svg = svg)}
+        >
           <rect
             x={this.state.rect.x}
             y={this.state.rect.y}
             width="20"
             height="20"
-            ref={(e) => (this.svgRectElem = e)}
+            ref={(e) => (this.svgRectElem = e ? e : undefined)}
             onMouseDown={(e) => this.startDrag(e, this.svgRectElem)}
           />
         </svg>
@@ -28,12 +38,13 @@ export default class Rect extends React.Component {
     );
   }
 
-  startDrag(event, draggedElem) {
+  startDrag(event: React.MouseEvent, draggedElem: SVGRectElement | undefined) {
     event.preventDefault();
+    if (this.svg === undefined) return;
     let point = this.svg.createSVGPoint();
     point.x = event.clientX;
     point.y = event.clientY;
-    point = point.matrixTransform(this.svg.getScreenCTM().inverse());
+    point = point.matrixTransform(this.svg.getScreenCTM()?.inverse());
     this.setState({
       dragOffset: {
         x: point.x - this.state.rect.x,
@@ -41,11 +52,11 @@ export default class Rect extends React.Component {
       },
     });
 
-    const mousemove = (event) => {
+    const mousemove = (event: MouseEvent) => {
       event.preventDefault();
       point.x = event.clientX;
       point.y = event.clientY;
-      let cursor = point.matrixTransform(this.svg.getScreenCTM().inverse());
+      let cursor = point.matrixTransform(this.svg?.getScreenCTM()?.inverse());
       this.setState({
         rect: {
           x: cursor.x - this.state.dragOffset.x,
@@ -54,7 +65,7 @@ export default class Rect extends React.Component {
       });
     };
 
-    const mouseup = (event) => {
+    const mouseup = (event: MouseEvent) => {
       document.removeEventListener("mousemove", mousemove);
       document.removeEventListener("mouseup", mouseup);
     };
